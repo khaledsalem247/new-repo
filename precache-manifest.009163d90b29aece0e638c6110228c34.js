3c9d01bd3449672c6ae6be20511435bf7eddd32c
khaledsalem247 <kkhaledsalem.247@gmail.com>
2020-09-30 17:30:34 +0200

    Updates

---
+++ precache-manifest.009163d90b29aece0e638c6110228c34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195cba61325504d5959a63fb7ab987f4",
+    "url": "/new-repo/index.html"
+  },
+  {
+    "revision": "8b452f77ae2f5eb6e7d5",
+    "url": "/new-repo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7ac67b295df339b14d65",
+    "url": "/new-repo/static/js/2.f329fbd9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new-repo/static/js/2.f329fbd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b452f77ae2f5eb6e7d5",
+    "url": "/new-repo/static/js/main.a5d8b3d7.chunk.js"
+  },
+  {
+    "revision": "d5e5230e001c22ce2ca1",
+    "url": "/new-repo/static/js/runtime-main.b245852e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new-repo/static/media/logo.5d5d9eef.svg"
+  }
+]);
